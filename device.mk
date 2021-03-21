88432b6afbf0ff0c861037f3e64a01489a0fe166
henricaodopao <allricknation@gmail.com>
2021-03-21 15:44:21 +0000

    Revert "X00QD: Build soundtriggerhal from source" bootloop lol
    This reverts commit 583a5e42a1d87c3b45dd1dbd88d4bb4df6987ed7.

--- device.mk
+++ device.mk
@@ -81,7 +81,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.1-impl \
     android.hardware.audio.common@2.0-util \
     android.hardware.audio.common@5.0-util \
     audio.a2dp.default \
@@ -108,8 +108,7 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     tinymix \
     libtinycompress \
-    libtinycompress.vendor \
-    sound_trigger.primary.sdm660
+    libtinycompress.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_configs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_configs.xml \
